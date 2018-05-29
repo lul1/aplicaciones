5351ad6c1ec6605c2c0d95f90b69c0f5afce7f94
kervic <kervicgalvez@gmail.com>
2018-05-29 10:59:33 -0500

    primer commit

--- app/src/main/java/com/kayak/toulouse/kayak/data/entities/FlyListItem.kt
+++ app/src/main/java/com/kayak/toulouse/kayak/data/entities/FlyListItem.kt
@@ -1,3 +1,9 @@
 package com.kayak.toulouse.kayak.data.entities
 
-data class FlyListItem()
+data class FlyListItem(
+        val origin: FlyListItemFly,
+        val destination: FlyListItemFly,
+        val price: Number,
+        val airline: String
+
+)
